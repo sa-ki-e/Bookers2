9ab3ad295231b7b5fc0803964de77fb5ba9d86a4
Sakie Kawamichi <sakie0303@gmail.com>
2022-02-13 23:26:48 +0000

    Bookモデル,テーブル作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,10 @@ class ApplicationController < ActionController::Base
     about_path
   end
     
+  def after_sign_out_path_for(resource)
+    about_path
+  end
+  
   protected
 
   def configure_permitted_parameters
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   
+  resources :users#, only: [:show, :index, :edit, :]
+ 
   root to: "homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/homes/about" => "homes#about", as: "about"
-  resources :users
 end
